b40f9b55de7cdf65d21138acdbfd50b5d9c274e6
Philip Phlek <pphlek@gmail.com>
2024-01-09 16:53:32 -0500

    initial project setup with graphql, mongo for project and client queries

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import 'colors'
+import * as dotenv from 'dotenv'
+import {graphqlHTTP} from 'express-graphql'
+import schema from './schema/schema'
+import connectDB from './config/db'
+
+dotenv.config()
+
+const port = process.env.PORT || 5000
+const app = express()
+
+connectDB()
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+)
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
---
+++ server/models/Client.ts
@@ -0,0 +1,9 @@
+import {Schema, model} from 'mongoose'
+
+const ClientSchema = new Schema({
+  name: {type: String},
+  email: {type: String},
+  phone: {type: String},
+})
+
+export default model('Client', ClientSchema)
